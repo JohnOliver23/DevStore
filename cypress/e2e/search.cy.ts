81f5d137e84f1f5b0369082e452d025491e7458f
JohnOliver23 <johnoliver021@gmail.com>
2025-05-23 14:51:34 -0300

    Criando testes de buscas de produtos

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,24 @@
+describe('search products', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to search for a products', () => {
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able tovisit search page without a search query', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+
+    cy.visit('/search')
+
+    cy.location('pathname').should('equal', '/')
+  })
+})
